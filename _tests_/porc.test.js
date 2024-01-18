c12a248ee26da8c0c9027c5f13bc799305062041
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-17 23:51:15 -0600

    Agregué una función para calcular porcentaje

---
+++ _tests_/porc.test.js
@@ -0,0 +1,24 @@
+// Esta función recibe dos números y devuelve el porcentaje del primero respecto al segundo
+function porcentaje(a, b) {
+    return (a / b) * 100; 
+}
+
+// En esta prueba esperamos que el porcentaje de 50 respecto a 200 sea igual a 25
+test('Porcentaje de un número respecto a otro', () => {
+    expect(porcentaje(50, 200)).toBe(25);
+});
+
+// En esta prueba esperamos que el porcentaje de 30 respecto a 120 sea igual a 25
+test('Porcentaje de un número respecto a otro', () => {
+    expect(porcentaje(30, 120)).toBe(25); 
+});
+
+// En esta prueba esperamos que el porcentaje de 200 respecto a 200 sea igual a 100
+test('Porcentaje de un número respecto a sí mismo', () => {
+    expect(porcentaje(200, 200)).toBe(100);
+});
+
+// En esta prueba esperamos que el porcentaje de 10 respecto a 0 lance un error
+test('Porcentaje respecto a cero', () => {
+    expect(() => porcentaje(10, 0)).toThrow();
+});
