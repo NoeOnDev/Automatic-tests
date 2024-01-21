6fa76b3836434af085479e78c047927301879b8c
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-20 23:24:46 -0600

    Eliminando comentarios de arrayMenor test

--- _tests_/arrayMenor.test.js
+++ _tests_/arrayMenor.test.js
@@ -1,19 +1,15 @@
-// Esta función recibe un array de números y devuelve el valor mínimo
 function minimo(array) {
     return Math.min(...array);
 }
 
-// En esta prueba esperamos que el valor mínimo de [1, 2, 3, 4, 5] sea 1
 test('Valor mínimo de un array', () => {
     expect(minimo([1, 2, 3, 4, 5])).toBe(1);
 });
 
-// En esta prueba esperamos que el valor mínimo de [-1, -2, -3, -4, -5] sea -5
 test('Valor mínimo de un array de números negativos', () => {
     expect(minimo([-1, -2, -3, -4, -5])).toBe(-5);
 });
 
-// En esta prueba esperamos que el valor mínimo de [0, 0, 0, 0, 0] sea 0
 test('Valor mínimo de un array de ceros', () => {
     expect(minimo([0, 0, 0, 0, 0])).toBe(0);
 });
