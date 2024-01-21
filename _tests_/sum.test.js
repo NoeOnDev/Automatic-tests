10dadaedbabc03db5a622098976784ddee9f8ff5
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-20 23:14:03 -0600

    Eliminando los cpmentarios de mi sum test

--- _tests_/sum.test.js
+++ _tests_/sum.test.js
@@ -1,4 +1,3 @@
-// Esta función recibe dos números y devuelve la suma de los mismos
 function sumar(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
       throw new Error("Los argumentos deben ser números");
@@ -6,30 +5,26 @@ function sumar(a, b) {
   return a + b; 
 }
  
-  // En esta prueba esperamos que la suma de 2 + 3 sea igual a 5
   test('Suma de dos números positivos', () => {
     expect(sumar(2, 3)).toBe(5);
   });
-  // En esta prueba esperamos que la suma de -3 + -6 sea igual a -9
+
   test('Suma de dos números negativos', () => {
     expect(sumar(-3, -6)).toBe(-9); 
   });
-  //En esta prueba esperamos que la suma de -10 + 5 sea igual a -5
+
   test('Suma de positivo y negativo', () => {
     expect(sumar(-10, 5)).toBe(-5);
   });
 
-  // En esta prueba esperamos que la suma de "2" y "3" lance un error
   test('Suma con valores no numéricos', () => {
     expect(() => sumar("2", "3")).toThrow();
   });
 
-  // En esta prueba esperamos que la suma de null y 3 lance un error
   test('Suma con valor null', () => {
     expect(() => sumar(null, 3)).toThrow();
   });
 
-  // En esta prueba esperamos que la suma de undefined y 3 lance un error
   test('Suma con valor undefined', () => {
     expect(() => sumar(undefined, 3)).toThrow();
   });
