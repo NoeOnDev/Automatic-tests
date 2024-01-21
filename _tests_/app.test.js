dc3188e06eed09f2ac0154aa9f832d6c01435825
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-20 23:19:04 -0600

    Eliminando cometarios de mi app test

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -1,22 +1,21 @@
 import request from "supertest";
 import app from "../app.js";
 
-// Realizo una pruba para el endpoint ('/'), que debe responder con un status 200
 describe("Prueba del endpoint ('/')", () => {
 
   let server;
-  // Antes de cada prueba, levanto el servidor
+
   beforeAll(() => {
      server = app.listen(5000);
   });
-   // Después de cada prueba, cierro el servidor
+
   afterAll(() => {
      server.close();
   })
-  // Realizo la prueba
+
   it("debe responder con un status 200 en la ruta ('/')", async () => {
     const res = await request(app).get("/");
-    // Espero que el status de la respuesta sea 200
+    
     expect(res.statusCode).toEqual(200);
   });
 });
