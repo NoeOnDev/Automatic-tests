548ac566af3ed86f3310a1836d225a34920d56d8
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-20 23:20:54 -0600

    Eliminando comentarios de mi array test

--- _tests_/array.test.js
+++ _tests_/array.test.js
@@ -1,19 +1,15 @@
-// Esta función recibe un array de números y devuelve el valor máximo
 function maximo(array) {
     return Math.max(...array);
 }
 
-// En esta prueba esperamos que el valor máximo de [1, 2, 3, 4, 5] sea 5
 test('Valor máximo de un array', () => {
     expect(maximo([1, 2, 3, 4, 5])).toBe(5);
 });
 
-// En esta prueba esperamos que el valor máximo de [-1, -2, -3, -4, -5] sea -1
 test('Valor máximo de un array de números negativos', () => {
     expect(maximo([-1, -2, -3, -4, -5])).toBe(-1);
 });
 
-// En esta prueba esperamos que el valor máximo de [0, 0, 0, 0, 0] sea 0
 test('Valor máximo de un array de ceros', () => {
     expect(maximo([0, 0, 0, 0, 0])).toBe(0);
 });
