daabc18013b04b5dc6be4b608d6cbc68bbcc12be
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-24 22:56:42 -0600

    Fix error handling in array.test.js

--- _tests_/array.test.js
+++ _tests_/array.test.js
@@ -37,9 +37,9 @@ test('Valor máximo de un array vacío', () => {
     expect(maximo([])).toBe(-Infinity);
 });
 
-// En esta prueba esperamos que el valor máximo de un array con números y cadenas de texto sea el número más grande
+// En esta prueba esperamos que el valor máximo de un array con números y cadenas de texto lance un error
 test('Valor máximo de un array con números y cadenas de texto', () => {
-    expect(maximo([1, 2, 3, '4', '5'])).toBe(5);
+    expect(() => maximo([1, 2, 3, '4', '5'])).toThrow("El array contiene una cadena de texto");
 });
 
 // En esta prueba esperamos que el valor máximo de un array con números y null sea el número más grande
@@ -47,17 +47,17 @@ test('Valor máximo de un array con números y null', () => {
     expect(maximo([1, 2, 3, null])).toBe(3);
 });
 
-// En esta prueba esperamos que el valor máximo de un array con números y undefined sea el número más grande
+// En esta prueba esperamos que el valor máximo de un array con números y undefined lance un error
 test('Valor máximo de un array con números y undefined', () => {
-    expect(maximo([1, 2, 3, undefined])).toBe(3);
+    expect(() => maximo([1, 2, 3, undefined])).toThrow("El array contiene NaN");
 });
 
-// En esta prueba esperamos que el valor máximo de un array con números y NaN sea el número más grande
+// En esta prueba esperamos que el valor máximo de un array con números y NaN lance un error
 test('Valor máximo de un array con números y NaN', () => {
-    expect(maximo([1, 2, 3, NaN])).toBe(3);
+    expect(() => maximo([1, 2, 3, NaN])).toThrow("El array contiene NaN");
 });
 
 // En esta prueba esperamos que el valor máximo de un array con solo cadenas de texto lance un error
 test('Valor máximo de un array con solo cadenas de texto', () => {
-    expect(() => maximo(['1', '2', '3'])).toThrow();
+    expect(() => maximo(['1', '2', '3'])).toThrow("El array contiene una cadena de texto");
 });
