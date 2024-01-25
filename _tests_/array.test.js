daa918c6a9b90743fbe27ea0e14bd6470d65d089
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-24 22:57:05 -0600

    Refactor array.test.js

--- _tests_/array.test.js
+++ _tests_/array.test.js
@@ -37,27 +37,22 @@ test('Valor máximo de un array vacío', () => {
     expect(maximo([])).toBe(-Infinity);
 });
 
-// En esta prueba esperamos que el valor máximo de un array con números y cadenas de texto lance un error
 test('Valor máximo de un array con números y cadenas de texto', () => {
     expect(() => maximo([1, 2, 3, '4', '5'])).toThrow("El array contiene una cadena de texto");
 });
 
-// En esta prueba esperamos que el valor máximo de un array con números y null sea el número más grande
 test('Valor máximo de un array con números y null', () => {
     expect(maximo([1, 2, 3, null])).toBe(3);
 });
 
-// En esta prueba esperamos que el valor máximo de un array con números y undefined lance un error
 test('Valor máximo de un array con números y undefined', () => {
     expect(() => maximo([1, 2, 3, undefined])).toThrow("El array contiene NaN");
 });
 
-// En esta prueba esperamos que el valor máximo de un array con números y NaN lance un error
 test('Valor máximo de un array con números y NaN', () => {
     expect(() => maximo([1, 2, 3, NaN])).toThrow("El array contiene NaN");
 });
 
-// En esta prueba esperamos que el valor máximo de un array con solo cadenas de texto lance un error
 test('Valor máximo de un array con solo cadenas de texto', () => {
     expect(() => maximo(['1', '2', '3'])).toThrow("El array contiene una cadena de texto");
 });
