2712255b8654d9dd01f17c4949f0137f7f860bde
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-01-25 23:49:42 -0600

    Refactor subtraction tests

--- _tests_/res.test.js
+++ _tests_/res.test.js
@@ -17,27 +17,22 @@ test('Resta de un número negativo y un número positivo', () => {
     expect(restar(-10, 5)).toBe(-15);
 });
 
-// En esta prueba esperamos que la resta de un número y cero sea el mismo número
 test('Resta de un número y cero', () => {
     expect(restar(5, 0)).toBe(5);
 });
 
-// En esta prueba esperamos que la resta de un número y el mismo número sea cero
 test('Resta de un número y el mismo número', () => {
     expect(restar(5, 5)).toBe(0);
 });
 
-// En esta prueba esperamos que la resta de un número y un número no numérico lance un error
 test('Resta de un número y un número no numérico', () => {
     expect(() => restar(5, "3")).toThrow();
 });
 
-// En esta prueba esperamos que la resta de un número y null lance un error
 test('Resta de un número y null', () => {
     expect(() => restar(5, null)).toThrow();
 });
 
-// En esta prueba esperamos que la resta de un número y undefined lance un error
 test('Resta de un número y undefined', () => {
     expect(() => restar(5, undefined)).toThrow();
 });
