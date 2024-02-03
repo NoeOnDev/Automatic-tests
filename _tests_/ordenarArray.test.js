cff117883c2e5679e65bbf436d432b3e87f7f3f3
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-02 22:11:00 -0600

    Añadir pruebas para ordenar un array de números con números decimales

--- _tests_/ordenarArray.test.js
+++ _tests_/ordenarArray.test.js
@@ -9,3 +9,7 @@ test('Ordenar un array de números', () => {
 test('Ordenar un array de números con números negativos', () => {
     expect(ordenarArray([5, -2, 1, -3, 4])).toEqual([-3, -2, 1, 4, 5]);
 });
+
+test('Ordenar un array de números con números decimales', () => {
+    expect(ordenarArray([5.5, 2.2, 1.1, 3.3, 4.4])).toEqual([1.1, 2.2, 3.3, 4.4, 5.5]);
+});
