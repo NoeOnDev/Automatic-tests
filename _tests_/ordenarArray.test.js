56d64161ddcca6d480221c5fb340680879da0982
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-02 22:10:45 -0600

    Añadir prueba para ordenar un array de números con números negativos

--- _tests_/ordenarArray.test.js
+++ _tests_/ordenarArray.test.js
@@ -5,3 +5,7 @@ function ordenarArray(array) {
 test('Ordenar un array de números', () => {
     expect(ordenarArray([5, 2, 1, 3, 4])).toEqual([1, 2, 3, 4, 5]);
 });
+
+test('Ordenar un array de números con números negativos', () => {
+    expect(ordenarArray([5, -2, 1, -3, 4])).toEqual([-3, -2, 1, 4, 5]);
+});
