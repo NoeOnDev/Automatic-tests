629012af582216c0614221a94942e127c1a28ead
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-02 22:10:25 -0600

    Añadir prueba para ordenar un array de números

--- _tests_/ordenarArray.test.js
+++ _tests_/ordenarArray.test.js
@@ -2,3 +2,6 @@ function ordenarArray(array) {
     return array.sort((a, b) => a - b);
 }
 
+test('Ordenar un array de números', () => {
+    expect(ordenarArray([5, 2, 1, 3, 4])).toEqual([1, 2, 3, 4, 5]);
+});
