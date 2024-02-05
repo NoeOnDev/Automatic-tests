69027a70166f7ad564c53d9e67e47ebbd95e54f0
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-04 20:56:36 -0600

    Agrega pruebas de servidor y cierra el servidor después de las pruebas

--- _tests_/app1.test.js
+++ _tests_/app1.test.js
@@ -1,9 +1,19 @@
-import supertest from 'supertest';
+import { request } from 'supertest';
 import app from '../app1.js';
+
+let server;
+
+beforeAll(async () => {
+    server = app.listen(9020); // Inicia el servidor antes de las pruebas
+});
+
+afterAll(async () => {
+    await server.close(); // Cierra el servidor después de las pruebas
+});
 
 describe('Test the /books endpoints', () => {
     test('It should respond to the POST method', async () => {
-        const response = await request(app)
+        const response = await request(server)
             .post('/books')
             .send({ title: 'Test Book', autor: 'Test Author', editorial: 'Test Editorial' });
 
@@ -14,7 +24,7 @@ describe('Test the /books endpoints', () => {
     });
 
     test('It should respond to the GET method', async () => {
-        const response = await request(app).get('/books');
+        const response = await request(server).get('/books');
 
         expect(response.statusCode).toBe(200);
         expect(Array.isArray(response.body)).toBe(true);
