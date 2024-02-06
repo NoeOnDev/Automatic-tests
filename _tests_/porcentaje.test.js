4a160fff1ea4f3e60cb05ac2704a1fc551519771
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-05 22:49:32 -0600

    Agregado cálculo de porcentaje para 100% en calcularPorcentaje()

--- _tests_/porcentaje.test.js
+++ _tests_/porcentaje.test.js
@@ -5,5 +5,6 @@ describe('calcularPorcentaje', () => {
     test('debería calcular el porcentaje correcto', () => {
         expect(calcularPorcentaje(200, 50)).toBe(25);
         expect(calcularPorcentaje(500, 125)).toBe(25);
+        expect(calcularPorcentaje(100, 100)).toBe(100);
     });
 });
