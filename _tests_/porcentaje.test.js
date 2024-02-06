bb7b1f13eb8a5abb9c18bd4b378a6a2ce7d16a7d
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-05 22:48:48 -0600

    Agregar prueba unitaria para calcularPorcentaje() en porcentaje.test.js

--- _tests_/porcentaje.test.js
+++ _tests_/porcentaje.test.js
@@ -1,3 +1,8 @@
 function calcularPorcentaje(total, cantidad) {
     return (cantidad / total) * 100;
 }
+describe('calcularPorcentaje', () => {
+    test('debería calcular el porcentaje correcto', () => {
+        expect(calcularPorcentaje(200, 50)).toBe(25);
+    });
+});
