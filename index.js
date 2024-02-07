fc988547d54af1743e04d65744b25967d3cdd6f2
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-06 19:41:31 -0600

    Agregada clase User para el modelo de usuarios

--- index.js
+++ index.js
@@ -30,3 +30,32 @@ sequelize.authenticate().then(() => { console.log('Authentication sussesful') })
  sequelize.sync({ force: true }).then(() => { console.log('Database sync') })
     .catch(err => { console.log('Error: ', err) });
 
+class User extends Model {}
+
+User.init({
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'users'
+});
