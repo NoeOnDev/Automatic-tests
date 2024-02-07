fbd8956496d354c56168af5891b25adebf7750bf
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-06 19:27:22 -0600

    Agrega configuración de servidor y rutas en index.js

--- index.js
+++ index.js
@@ -4,5 +4,22 @@ import { Sequelize, Model, DataTypes } from 'sequelize';
 
 config();
 
-const { BD_NAME, DB_USER, DB_PASS, DB_PORT, DB_HOST } = process.env;
+const { PORT, BD_NAME, DB_USER, DB_PASS, DB_PORT, DB_HOST } = process.env;
+const app = express();
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+const sequelize = new Sequelize(BD_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
+    dialect: 'postgres',
+    port: DB_PORT,
+    logging: false
+});
