5f30f4a9274026920c9f2b92503ad380ff9936cd
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-07 22:17:50 -0600

    Agregada lógica para eliminar usuarios

--- index.js
+++ index.js
@@ -73,7 +73,17 @@ app.put('/user', verifyToken, async (req, res) => {
     }
 });
 
-app.delete('/user', verifyToken, async (req, res) => {});
+app.delete('/user', verifyToken, async (req, res) => {
+    try {
+        const user = await User.findByPk(req.user.userId);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+    } catch (error) {
+
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
